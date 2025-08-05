856634428e15a2a3e53938d994b26b377dd2bfae
aqsashuaib21 <aqsashuaib077@gmail.com>
2025-08-05 13:34:22 +0530

    Implement request-header-parser

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
 var express = require('express');
 var app = express();
@@ -19,12 +15,16 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
 
-// listen for requests :)
+app.get("/api/whoami",(req,res)=>{
+  const ipaddress=req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress;
+ res.json({ipaddress,
+  language:req.headers['accept-language'],
+software:req.headers['user-agent']}
+ )
+})
+
+
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
